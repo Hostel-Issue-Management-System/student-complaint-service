1f4fe8548799fffcedb789937c705320b7ebf69b
Prithviraj Naik <prithvirajnaik17@gmail.com>
2023-05-08 11:41:04 +0530

    Update StudentComplaintServiceApplicationTests.java

--- src/test/java/com/example/studentcomplaintservice/StudentComplaintServiceApplicationTests.java
+++ src/test/java/com/example/studentcomplaintservice/StudentComplaintServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StudentComplaintServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
 }
